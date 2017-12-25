5acf3aeff489cd84e8a4d3a0baee1fcafa53931e
jzySaber1996 <529930262@qq.com>
2017-12-25 13:09:06 +0800

    子祥部分SeminarController

--- SeminarController.cs
+++ SeminarController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Xmu.Crms.Shared.Exceptions;
 using Xmu.Crms.Shared.Models;
 using Xmu.Crms.Shared.Service;
+using System.Linq;
+using Xmu.Crms.Shared.Exceptions;
 using Type = Xmu.Crms.Shared.Models.Type;
 
 namespace Xmu.Crms.Insomnia
@@ -17,7 +19,8 @@ namespace Xmu.Crms.Insomnia
     {
         private readonly ISeminarService _seminarService;
         private readonly ITopicService _topicService;
-
+        private readonly IUserService _userService;
+        private readonly ISeminarGroupService _seminargroupService;
         public SeminarController(ISeminarService seminarService, ITopicService topicService)
         {
             _seminarService = seminarService;
@@ -36,31 +39,17 @@ namespace Xmu.Crms.Insomnia
                 return StatusCode(404, new { msg = "讨论课不存在" });
 
             }
-        }
-
-        [HttpDelete("/seminar/{seminarId:long}")]
-        public IActionResult DeleteSeminarById([FromRoute] long seminarId)
-        {
-            if (User.Type() != Type.Teacher)
+            catch (ArgumentException)
             {
-                return StatusCode(403, new { msg = "权限不足" });
-            }
-
-            try
-            {
-                _seminarService.DeleteSeminarBySeminarId(seminarId);
-                return NoContent();
-            }
-            catch (SeminarNotFoundException)
-            {
-                return StatusCode(404, new { msg = "讨论课不存在" });
+                return StatusCode(400, new { msg = "讨论课ID输入格式有误" });
             }
         }
 
         [HttpPut("/seminar/{seminarId:long}")]
         public IActionResult UpdateSeminarById([FromRoute] long seminarId, [FromBody] Seminar updated)
         {
-            if (User.Type() != Type.Teacher)
+            var userlogin = _userService.GetUserByUserId(User.Id());
+            if (userlogin.Type != Type.Teacher)
             {
                 return StatusCode(403, new { msg = "权限不足" });
             }
@@ -74,31 +63,118 @@ namespace Xmu.Crms.Insomnia
             {
                 return StatusCode(404, new { msg = "讨论课不存在" });
             }
+            catch (ArgumentException)
+            {
+                return StatusCode(400, new { msg = "讨论课ID输入格式有误" });
+            }
         }
-
-        [HttpGet("/seminar/{seminarId:long}/topic")]
-        public IActionResult GetTopicsBySeminarId([FromRoute] long seminarId)
+        [HttpDelete("/seminar/{seminarId:long}")]
+        public IActionResult DeleteSeminarById([FromRoute] long seminarId)
         {
+            var userlogin = _userService.GetUserByUserId(User.Id());
+            if (userlogin.Type != Type.Teacher)
+            {
+                return StatusCode(403, new { msg = "权限不足" });
+            }
             try
             {
-                return Json(_topicService.ListTopicBySeminarId(seminarId));
+                _seminarService.DeleteSeminarBySeminarId(seminarId);
+                return NoContent();
             }
             catch (SeminarNotFoundException)
             {
                 return StatusCode(404, new { msg = "讨论课不存在" });
             }
+            catch (ArgumentException)
+            {
+                return StatusCode(400, new { msg = "讨论课ID输入格式有误" });
+            }
+        }
+
+
+
+
+
+
+
+
+        //serial  groupLeft未加
+        [HttpGet("/seminar/{seminarId:long}/topic")]
+        public IActionResult GetTopicsBySeminarId([FromRoute] long seminarId)
+        {
+            /*
+             * "id": 257,
+    "serial": "A",
+    "name": "领域模型与模块",
+    "description": "Domain model与模块划分",
+    "groupLimit": 5,
+    "groupMemberLimit": 6,
+    "groupLeft": 2
+             */
+
+            try
+            {
+                var topics = _topicService.ListTopicBySeminarId(seminarId);
+                return Json(topics.Select(t => new
+                {
+                    id = t.Id,
+                    name = t.Name,
+                    description = t.Description,
+                    groupLimit = t.GroupNumberLimit,
+                    groupMemberLimit = t.GroupStudentLimit,
+
+                }));
+            }
+            catch (SeminarNotFoundException)
+            {
+                return StatusCode(404, new { msg = "讨论课不存在" });
+            }
+            catch (ArgumentException)
+            {
+                return StatusCode(400, new { msg = "话题ID输入格式有误" });
+            }
         }
 
         [HttpPut("/seminar/{seminarId:long}/topic")]
-        public IActionResult CreateTopicBySeminarId([FromRoute] long seminarId, [FromBody] Seminar newSeminar)
+        public IActionResult CreateTopicBySeminarId([FromRoute] long seminarId, [FromBody] Topic newTopic)
         {
-            return Created("/topic/1", newSeminar);
+            var userlogin = _userService.GetUserByUserId(User.Id());
+            if (userlogin.Type == Shared.Models.Type.Teacher)
+            {
+                var topicid = _topicService.InsertTopicBySeminarId(seminarId, newTopic);
+                return Created("/topic/" + topicid, newTopic);
+            }
+            return StatusCode(403, new { msg = "权限不足" });
         }
 
+
+
+
+
+        //没有小组成员 和 report
         [HttpGet("/seminar/{seminarId:long}/group")]
         public IActionResult GetGroupsBySeminarId([FromRoute] long seminarId)
         {
-            return Json(new List<SeminarGroup>(), Utils.Ignoring("Group*", "Members", "Leader", "Report", "Grade"));
+            try
+            {
+                var groups = _seminargroupService.ListSeminarGroupBySeminarId(seminarId);
+                return Json(groups.Select(t => new
+                {
+                    id = t.Id,
+                    leader = t.Leader,
+                    presentationgrade = t.PresentationGrade,
+                    reportgrade = t.ReportGrade,
+                    finalgrade = t.FinalGrade,
+                }));
+            }
+            catch (SeminarNotFoundException)
+            {
+                return StatusCode(404, new { msg = "讨论课不存在" });
+            }
+            catch (ArgumentException)
+            {
+                return StatusCode(400, new { msg = "讨论课ID输入格式有误" });
+            }
         }
     }
 }
